0ce7179edee4eba53120c0b0d88df86b6f34425b
Vectorial1024 <vincentwwy168@gmail.com>
2021-04-12 00:16:45 +0800

    Revert "Close the Harmony leakage"
    This reverts commit d9b9dc81695aff6fdc55919b1dc650602113465f.

--- SpecializedIndustryFixRedux/PatchController.cs
+++ SpecializedIndustryFixRedux/PatchController.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace SpecializedIndustryFixRedux
 {
-    internal class PatchController
+    public class PatchController
     {
         public static string HarmonyModID
         {
